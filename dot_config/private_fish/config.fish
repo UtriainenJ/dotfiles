addfe28ae6d643887ec8910407bc28b23be723ed
Jaakko Utriainen <jaakko.utriainen@tuni.fi>
2025-12-18 19:32:36 +0200

    add fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    #found in .bash_profile, adjusted for fish
+    dbus-update-activation-environment --systemd --all
+    # Aliases
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    alias bye='systemctl poweroff'
+    alias shut='systemctl poweroff'
+    alias yeet='sudo pacman -Rns'
+
+    # fuzzy search file to ctrl+f and variables to alt+ctrl+v
+    fzf_configure_bindings --directory=\cf --variables=\e\cv
+
+end
+
+############################
+# PATH additions (prepend) #
+############################
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
